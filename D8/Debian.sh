3042cd0270400a92895234799a8e3646150a8b08
Chuck <nelsoch@pm.me>
2018-08-23 02:15:53 -0700

    Many use D7, but we should try to use Drupal 8

---
+++ D8/Debian.sh
@@ -0,0 +1,15 @@
+!#/bin/bash
+cd /var/www/
+
+// install stuff
+apt-get update && apt-get upgrade -y
+apt-get install -y git apache2 drush fail2ban ufw
+drush dl drupal-8.x
+
+//basic security
+ufw allow 80 && ufw allow 22 && ufw allow 443 && ufw enable
+service fail2ban start
+
+//blast-off
+service apcahe2 start
+mv drupal* /var/www/drupal
